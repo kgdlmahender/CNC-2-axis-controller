b1a03150025c1d8f6766dfb02fe56c79781e952b
kodigudla mahender <kgdl.mahender1997@gmail.com>
2024-12-20 01:00:17 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.distanceTextBox = New System.Windows.Forms.TextBox()
+		Me.movecontinous_radiobutton = New System.Windows.Forms.RadioButton()
+		Me.movedistance_radiobutton = New System.Windows.Forms.RadioButton()
+		Me.xPlusButton = New System.Windows.Forms.Button()
+		Me.xMinusButton = New System.Windows.Forms.Button()
+		Me.yPlusButton = New System.Windows.Forms.Button()
+		Me.yMinusButton = New System.Windows.Forms.Button()
+		Me.openGCodeButton = New System.Windows.Forms.Button()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.graphPanel = New System.Windows.Forms.Panel()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.GroupBox1.SuspendLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.distanceTextBox)
+		Me.GroupBox1.Controls.Add(Me.movecontinous_radiobutton)
+		Me.GroupBox1.Controls.Add(Me.movedistance_radiobutton)
+		Me.GroupBox1.Location = New System.Drawing.Point(453, 12)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(221, 116)
+		Me.GroupBox1.TabIndex = 0
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "GroupBox1"
+		'
+		'distanceTextBox
+		'
+		Me.distanceTextBox.Location = New System.Drawing.Point(121, 33)
+		Me.distanceTextBox.Name = "distanceTextBox"
+		Me.distanceTextBox.Size = New System.Drawing.Size(100, 22)
+		Me.distanceTextBox.TabIndex = 2
+		'
+		'movecontinous_radiobutton
+		'
+		Me.movecontinous_radiobutton.AutoSize = True
+		Me.movecontinous_radiobutton.Location = New System.Drawing.Point(6, 74)
+		Me.movecontinous_radiobutton.Name = "movecontinous_radiobutton"
+		Me.movecontinous_radiobutton.Size = New System.Drawing.Size(122, 20)
+		Me.movecontinous_radiobutton.TabIndex = 1
+		Me.movecontinous_radiobutton.TabStop = True
+		Me.movecontinous_radiobutton.Text = "Move continuos"
+		Me.movecontinous_radiobutton.UseVisualStyleBackColor = True
+		'
+		'movedistance_radiobutton
+		'
+		Me.movedistance_radiobutton.AutoSize = True
+		Me.movedistance_radiobutton.Location = New System.Drawing.Point(6, 33)
+		Me.movedistance_radiobutton.Name = "movedistance_radiobutton"
+		Me.movedistance_radiobutton.Size = New System.Drawing.Size(113, 20)
+		Me.movedistance_radiobutton.TabIndex = 0
+		Me.movedistance_radiobutton.TabStop = True
+		Me.movedistance_radiobutton.Text = "Movedistance"
+		Me.movedistance_radiobutton.UseVisualStyleBackColor = True
+		'
+		'xPlusButton
+		'
+		Me.xPlusButton.Location = New System.Drawing.Point(12, 309)
+		Me.xPlusButton.Name = "xPlusButton"
+		Me.xPlusButton.Size = New System.Drawing.Size(75, 23)
+		Me.xPlusButton.TabIndex = 3
+		Me.xPlusButton.Text = "X+"
+		Me.xPlusButton.UseVisualStyleBackColor = True
+		'
+		'xMinusButton
+		'
+		Me.xMinusButton.Location = New System.Drawing.Point(115, 309)
+		Me.xMinusButton.Name = "xMinusButton"
+		Me.xMinusButton.Size = New System.Drawing.Size(75, 23)
+		Me.xMinusButton.TabIndex = 4
+		Me.xMinusButton.Text = "X-"
+		Me.xMinusButton.UseVisualStyleBackColor = True
+		'
+		'yPlusButton
+		'
+		Me.yPlusButton.Location = New System.Drawing.Point(216, 309)
+		Me.yPlusButton.Name = "yPlusButton"
+		Me.yPlusButton.Size = New System.Drawing.Size(75, 23)
+		Me.yPlusButton.TabIndex = 5
+		Me.yPlusButton.Text = "Y+"
+		Me.yPlusButton.UseVisualStyleBackColor = True
+		'
+		'yMinusButton
+		'
+		Me.yMinusButton.Location = New System.Drawing.Point(332, 309)
+		Me.yMinusButton.Name = "yMinusButton"
+		Me.yMinusButton.Size = New System.Drawing.Size(75, 23)
+		Me.yMinusButton.TabIndex = 6
+		Me.yMinusButton.Text = "Y-"
+		Me.yMinusButton.UseVisualStyleBackColor = True
+		'
+		'openGCodeButton
+		'
+		Me.openGCodeButton.Location = New System.Drawing.Point(442, 309)
+		Me.openGCodeButton.Name = "openGCodeButton"
+		Me.openGCodeButton.Size = New System.Drawing.Size(75, 23)
+		Me.openGCodeButton.TabIndex = 7
+		Me.openGCodeButton.Text = "Open GCode"
+		Me.openGCodeButton.UseVisualStyleBackColor = True
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Location = New System.Drawing.Point(49, 65)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(281, 154)
+		Me.PictureBox1.TabIndex = 8
+		Me.PictureBox1.TabStop = False
+		'
+		'graphPanel
+		'
+		Me.graphPanel.Location = New System.Drawing.Point(65, 86)
+		Me.graphPanel.Name = "graphPanel"
+		Me.graphPanel.Size = New System.Drawing.Size(200, 100)
+		Me.graphPanel.TabIndex = 9
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(44, 3)
+		Me.TextBox1.Multiline = True
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(286, 278)
+		Me.TextBox1.TabIndex = 10
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(401, 204)
+		Me.TextBox2.Multiline = True
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(221, 47)
+		Me.TextBox2.TabIndex = 11
+		Me.TextBox2.Text = "C:\Users\kgdlm\Downloads\linearmove_test.txt"
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(686, 361)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.graphPanel)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.Controls.Add(Me.openGCodeButton)
+		Me.Controls.Add(Me.yMinusButton)
+		Me.Controls.Add(Me.yPlusButton)
+		Me.Controls.Add(Me.xMinusButton)
+		Me.Controls.Add(Me.xPlusButton)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents distanceTextBox As TextBox
+	Friend WithEvents movecontinous_radiobutton As RadioButton
+	Friend WithEvents movedistance_radiobutton As RadioButton
+	Friend WithEvents xPlusButton As Button
+	Friend WithEvents xMinusButton As Button
+	Friend WithEvents yPlusButton As Button
+	Friend WithEvents yMinusButton As Button
+	Friend WithEvents openGCodeButton As Button
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents graphPanel As Panel
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,223 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Drawing
+
+Public Class Form1
+  Dim moveContinuous As Boolean = False
+  Dim moveDistance As Boolean = False
+  Dim xCurrent As Single = 0
+  Dim yCurrent As Single = 0
+  Dim path As New List(Of PointF)
+  Dim context As String
+  Private Const StepsPerRevolution As Integer = 400
+  Private Const PitchCircleDiameter As Double = 6.0
+  Dim WithEvents serialPort1 As New SerialPort()
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    ' Configure the serial port
+    SerialPort1.BaudRate = 9600
+    serialPort1.PortName = "COM24" ' Replace with your actual COM port
+    serialPort1.DataBits = 8
+    SerialPort1.Parity = Parity.None
+    SerialPort1.StopBits = StopBits.One
+    SerialPort1.Handshake = Handshake.None
+    SerialPort1.Encoding = System.Text.Encoding.Default
+
+    ' Open the serial port
+    SerialPort1.Open()
+    PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+  End Sub
+
+  ' Event handler for when data is received
+  Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles serialPort1.DataReceived
+    Dim data As String = serialPort1.ReadLine() ' Read the incoming data
+    UpdateTextBox(data) ' Update the UI
+  End Sub
+
+  ' Method to update the UI
+  Private Sub UpdateTextBox(data As String)
+    If InvokeRequired Then
+      Invoke(New Action(Of String)(AddressOf UpdateTextBox), data)
+    Else
+      TextBox1.AppendText(data & Environment.NewLine)
+    End If
+  End Sub
+
+  Private Sub movecontinous_radiobutton_CheckedChanged(sender As Object, e As EventArgs) Handles movecontinous_radiobutton.CheckedChanged
+    moveContinuous = movecontinous_radiobutton.Checked
+    moveDistance = Not moveContinuous
+  End Sub
+
+  Private Sub movedistance_radiobutton_CheckedChanged(sender As Object, e As EventArgs) Handles movedistance_radiobutton.CheckedChanged
+    moveDistance = movedistance_radiobutton.Checked
+    moveContinuous = Not moveDistance
+  End Sub
+
+  Private Sub Button_MouseDown(sender As Object, e As MouseEventArgs) Handles xPlusButton.MouseDown, xMinusButton.MouseDown, yPlusButton.MouseDown, yMinusButton.MouseDown
+    Dim command As String = ""
+    Dim direction As String = CType(sender, Button).Name
+    Dim readsignalfromarduoino As String
+
+    Select Case direction
+      Case "xPlusButton"
+        command = "X+"
+      Case "xMinusButton"
+        command = "X-"
+      Case "yPlusButton"
+        command = "Y+"
+      Case "yMinusButton"
+        command = "Y-"
+    End Select
+
+    If moveContinuous Then
+
+
+      SerialPort1.WriteLine("MOVE" & command)
+      ' context = "MOVE" & command
+      'readsignalfromarduoino = SerialPort1.ReadExisting(4)
+
+
+      'TextBox1.Text = readsignalfromarduoino
+    ElseIf moveDistance Then
+      Dim distance As String = distanceTextBox.Text
+      SerialPort1.WriteLine("DISTANCE" & command & distance)
+      'readsignalfromarduoino = SerialPort1.ReadExisting(4)
+
+
+      'TextBox1.Text = readsignalfromarduoino
+
+    End If
+  End Sub
+
+  Private Sub Button_MouseUp(sender As Object, e As MouseEventArgs) Handles xPlusButton.MouseUp, xMinusButton.MouseUp, yPlusButton.MouseUp, yMinusButton.MouseUp
+    If moveContinuous Then
+      SerialPort1.WriteLine("STOP")
+      'TextBox1.Text = "MouseUp"
+    End If
+  End Sub
+
+  Private Function CalculateSteps(distance As Double) As Integer
+    Dim circumference As Double = Math.PI * PitchCircleDiameter
+    Dim revolutions As Double = distance / circumference
+    ' TextBox1.Text = "steps"
+    'TextBox1.Text = revolutions * StepsPerRevolution
+
+    Return CInt(revolutions * StepsPerRevolution)
+
+
+  End Function
+
+  Private Sub openGcodeButton_Click(sender As Object, e As EventArgs) Handles openGCodeButton.Click
+    Try
+      Dim filePath As String = TextBox2.Text
+      If File.Exists(filePath) Then
+        Dim gCodeLines As String() = File.ReadAllLines(filePath)
+        For Each line As String In gCodeLines
+          Dim stepsToMove As Integer = ParseGCode(line)
+          If stepsToMove > 0 Or stepsToMove < 0 Then
+            If Not serialPort1.IsOpen Then
+              serialPort1.Open()
+            End If
+            serialPort1.WriteLine(stepsToMove.ToString())
+            ' Delay to allow the motor to complete the movement
+            System.Threading.Thread.Sleep(500)
+          End If
+        Next
+      Else
+        MessageBox.Show("File not found. Please enter a valid file path.")
+      End If
+    Catch ex As Exception
+      MessageBox.Show("Error processing G-code file: " & ex.Message)
+    End Try
+  End Sub
+
+  Private Function ParseGCode(line As String) As Integer
+    Dim stepsToMove As Integer
+    If line.StartsWith("G01") OrElse line.StartsWith("G1") Then
+      Dim parts As String() = line.Split(" ")
+
+      For Each part As String In parts
+
+        If part.StartsWith("X") Then
+          Dim distance As Double
+          If Double.TryParse(part.Substring(1), distance) Then
+
+
+
+            stepsToMove = CalculateSteps(distance)
+
+          End If
+        End If
+      Next
+    End If
+    'TextBox1.Text = stepsToMove
+    Return stepsToMove
+  End Function
+
+  'Private Sub openGcodeButton_Click(sender As Object, e As EventArgs) Handles openGCodeButton.Click
+  '  Dim openFileDialog As New OpenFileDialog
+  '  openFileDialog.Filter = "G-code Files|*.nc;*.txt"
+
+  '  If openFileDialog.ShowDialog() = DialogResult.OK Then
+  '    Dim lines As String() = File.ReadAllLines(openFileDialog.FileName)
+  '    path.Clear()
+  '    xCurrent = 0
+  '    yCurrent = 0
+  '    For Each line In lines
+  '      parseGCode(line)
+  '      'DrawPath()
+  '      System.Threading.Thread.Sleep(100)  ' Allow Arduino time to process each command
+  '    Next
+  '  End If
+  'End Sub
+
+
+  'Private Sub parseGCode(gcodeLine As String)
+  '  gcodeLine = gcodeLine.Trim()
+
+  '  If gcodeLine.StartsWith("G0") Or gcodeLine.StartsWith("G1") Then
+  '    Dim xIndex As Integer = gcodeLine.IndexOf("X"c)
+  '    Dim yIndex As Integer = gcodeLine.IndexOf("Y"c)
+
+  '    If xIndex <> -1 Then
+  '      Dim spaceIndex As Integer = gcodeLine.IndexOf(" ", xIndex)
+  '      If spaceIndex = -1 Then spaceIndex = gcodeLine.Length
+  '      Dim xDistance As Single = Val(gcodeLine.Substring(xIndex + 1, spaceIndex - xIndex - 1))
+  '      xCurrent += xDistance
+  '      serialPort1.WriteLine("G1 X" & xDistance) ' Send to Arduino
+  '    End If
+
+  '    If yIndex <> -1 Then
+  '      Dim spaceIndex As Integer = gcodeLine.IndexOf(" ", yIndex)
+  '      If spaceIndex = -1 Then spaceIndex = gcodeLine.Length
+  '      Dim yDistance As Single = Val(gcodeLine.Substring(yIndex + 1, spaceIndex - yIndex - 1))
+  '      yCurrent += yDistance
+  '      serialPort1.WriteLine("G1 Y" & yDistance) ' Send to Arduino
+  '    End If
+
+  '    path.Add(New PointF(xCurrent, yCurrent))
+  '  End If
+  'End Sub
+
+  Private Sub DrawPath()
+    Dim bmp As New Bitmap(PictureBox1.Width, PictureBox1.Height)
+    Dim g As Graphics = Graphics.FromImage(bmp)
+
+    ' Draw the path
+    If path.Count > 1 Then
+      For i As Integer = 0 To path.Count - 2
+        g.DrawLine(Pens.Red, path(i), path(i + 1))
+      Next
+    End If
+
+    ' Draw the current position
+    If path.Count > 0 Then
+      g.FillEllipse(Brushes.Blue, path.Last().X - 5, path.Last().Y - 5, 10, 10)
+    End If
+
+    PictureBox1.Image = bmp
+    g.Dispose()
+  End Sub
+
+
+End Class
